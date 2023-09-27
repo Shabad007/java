27e8fa6fe447d64635df7fb69b989de53a0eb4b3
Shabad007 <145149287+Shabad007@users.noreply.github.com>
2023-09-27 13:03:21 -0400

    Create greater.java

---
+++ greater.java
@@ -0,0 +1,16 @@
+//Program to find the largest of twi numberl
+class Main {
+  public static void main(String[] args) {
+    int x=10; int y=20;
+    if(x>y) //if block
+    { //true satement
+      System.out.println("x is greater");
+      
+    }
+    else //else block
+    { //false statement
+      System.out.println("y is greater");
+      
+    }
+  }
+}
