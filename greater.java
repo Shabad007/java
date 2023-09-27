90100bab17f3e0bac387b720556c5b24e11de7b1
Shabad007 <145149287+Shabad007@users.noreply.github.com>
2023-09-27 13:14:20 -0400

    Update greater.java

--- greater.java
+++ greater.java
@@ -1,7 +1,7 @@
-//Program to find the largest of twi numberl
+//Program to find the largest of two number
 class Main {
   public static void main(String[] args) {
-    int x=10; int y=20;
+    /*int x=10; int y=20;
     if(x>y) //if block
     { //true satement
       System.out.println("x is greater");
@@ -9,8 +9,19 @@ class Main {
     }
     else //else block
     { //false statement
-      System.out.println("y is greater");
+      System.out.println("x is less");
       
+    }*/
+    int num =10;
+
+      if(num%2==0)
+    {
+      System.out.println( num +" is an Even number");
+    
+    }
+    else
+    {
+      System.out.println( num +" is an Odd number");
     }
   }
 }
