40877caf1d996090d77d779f005614d6498da49b
Shabad007 <145149287+Shabad007@users.noreply.github.com>
2023-10-11 10:03:25 -0400

    Create greaterlessscanner.java

---
+++ greaterlessscanner.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter two numbers: ");
+    double num1 = input.nextDouble();
+    double num2 = input.nextDouble();
+    if (num1 > num2) {
+    System.out.println("The first number is greater");
+    } else {
+    System.out.println("The second number is         greater");
+    }
+  input.close();
+  }
+}
