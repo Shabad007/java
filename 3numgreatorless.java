2d1c6c2dd531e42c964f4b5475e3ac5218c9fe24
Shabad007 <145149287+Shabad007@users.noreply.github.com>
2023-10-16 21:27:00 -0400

    Create 3numgreatorless.java

---
+++ 3numgreatorless.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner input =new Scanner(System.in);
+    System.out.print("Enter three numbers: ");
+    double num1 = input.nextDouble();
+    double num2 = input.nextDouble();
+    double num3 = input.nextDouble();
+    if (num1<num2){
+    if (num1<num3){
+    System.out.println("number 1 is the smallest");
+        }
+    } else {   
+      if(num2<num3){
+      System.out.println("number 2 is smallest");
+          } else {
+        System.out.println("number 3 is the smallest");
+      }
+  }
+    input.close();
+  }
+}
