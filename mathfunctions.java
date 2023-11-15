465890c035b1e6a521ebb8ca700be93a65800e1a
Shabad007 <145149287+Shabad007@users.noreply.github.com>
2023-11-15 13:14:21 -0500

    Create mathfunctions.java

---
+++ mathfunctions.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+
+    double x = 9.0, y= 10.0, z;
+
+    z = Math.sqrt(36.0);
+    System.out.printf("z = %.1f\n", z);
+
+    z = Math.sqrt(x);
+    System.out.printf("z = %.1f\n", z);
+
+    z = Math.sqrt(x+y);
+    System.out.printf("z = %.1f\n", z);
+
+    System.out.printf("Square root of 100: %.1f\n", Math.sqrt(100.0));
+  }
+}
