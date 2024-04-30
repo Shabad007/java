0d12ef31a599bdad26999b179c19988d3aad080e
Shabad007 <145149287+Shabad007@users.noreply.github.com>
2024-04-30 15:26:32 -0400

    Create question12b.java

---
+++ question12b.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+   
+        System.out.println("Odd numbers using a while loop:");
+        int num = 1;
+        while (num <= 20) {
+            System.out.println(num);
+            num += 2;
+        }
+    }
+}
