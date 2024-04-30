1300650e15fff57d4880beb63c4f9465c545a2d0
Shabad007 <145149287+Shabad007@users.noreply.github.com>
2024-04-30 15:27:07 -0400

    Create question12c.java

---
+++ question12c.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+       
+        System.out.println("Even numbers using a do-while loop:");
+        int num = 2;
+        do {
+            System.out.println(num);
+            num += 2;
+        } while (num <= 20);
+    }
+}
