fb0aaa8674c464db7c85484d73c67cd921413068
Shabad007 <145149287+Shabad007@users.noreply.github.com>
2024-04-30 15:24:00 -0400

    Create question11.java

---
+++ question11.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+       
+        int rows = 5;
+        int columns = 8; 
+
+        for (int i = 0; i < rows; i++) {
+           
+            for (int j = 0; j < columns; j++) {
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+    }
+}
