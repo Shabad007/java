233db91531488c23d9b5f982fc89c4094254490a
Shabad007 <145149287+Shabad007@users.noreply.github.com>
2024-04-30 14:56:03 -0400

    Create question4.java

---
+++ question4.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        double sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+
+        double difference = num1 - num2;
+        System.out.println("Difference: " +    difference);
+        
+        double product = num1 * num2;
+        System.out.println("Product: " + product);
+
+   
+        if (num2 != 0) {
+            double quotient = num1 / num2;
+            System.out.println("Quotient: " + quotient);
+        } else {
+            System.out.println("Cannot divide by zero.");
+        }
+         
+
+        scanner.close();
+    }
+}
